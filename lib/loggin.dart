c58a80c8979e583d02bca510c47693ea599d3ac2
Matias <jorgematuavi45@gmail.com>
2024-09-08 19:39:10 -0300

    modificacion

--- lib/loggin.dart
+++ lib/loggin.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'agente sanitario/agent_screen.dart'; // Asegúrate de importar el archivo donde definiste HomeScreen
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +12,7 @@ class LoginScreen extends StatelessWidget {
           gradient: LinearGradient(
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
-            colors: [Colors.blue[100]!, Color.fromARGB(255, 197, 226, 250)!],
+            colors: [Colors.blue[100]!, const Color.fromARGB(255, 197, 226, 250)],
           ),
         ),
         child: Center(
@@ -20,38 +22,37 @@ class LoginScreen extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FlutterLogo(size: 100),
-                  SizedBox(height: 50),
+                  const FlutterLogo(size: 100),
+                  const SizedBox(height: 50),
                   TextField(
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.white,
                       hintText: 'Correo electrónico',
-                      prefixIcon: Icon(Icons.email),
+                      prefixIcon: const Icon(Icons.email),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
                     ),
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   TextField(
                     obscureText: true,
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.white,
                       hintText: 'Contraseña',
-                      prefixIcon: Icon(Icons.lock),
+                      prefixIcon: const Icon(Icons.lock),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
                     ),
                   ),
-                  SizedBox(height: 30),
+                  const SizedBox(height: 30),
                   ElevatedButton(
-                    child: Text('Iniciar Sesión', style: TextStyle(fontSize: 18)),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 168, 209, 250),
-                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      backgroundColor: const Color.fromARGB(255, 168, 209, 250),
+                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
@@ -62,10 +63,11 @@ class LoginScreen extends StatelessWidget {
                         MaterialPageRoute(builder: (context) => AgentScreen()),
                       );
                     },
+                    child: Text('Iniciar Sesión', style: TextStyle(fontSize: 18)),
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   TextButton(
-                    child: Text('¿Olvidaste tu contraseña?', style: TextStyle(color: const Color.fromARGB(255, 17, 0, 0))),
+                    child: const Text('¿Olvidaste tu contraseña?', style: TextStyle(color: Color.fromARGB(255, 17, 0, 0))),
                     onPressed: () {
                       // Aquí iría la lógica para recuperar la contraseña
                     },
